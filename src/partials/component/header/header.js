0d33105135faad7b9ed919e60b116d32f981b915
Gaondarech726 <anton.honcharenko2202@gmail.com>
2024-11-03 18:43:31 +0200

    add hover animation

--- src/partials/component/header/header.js
+++ src/partials/component/header/header.js
@@ -11,3 +11,40 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const navLinks = document.querySelectorAll('.nav-link');
+  const activeLink = document.querySelector('.nav-link.active');
+  let currentActiveLink = activeLink;
+
+  const ball = document.createElement('span');
+  ball.classList.add('ball');
+  document.querySelector('.categories').appendChild(ball);
+
+  moveBallTo(currentActiveLink);
+
+  navLinks.forEach(link => {
+    link.addEventListener('mouseenter', () => {
+      moveBallTo(link);
+    });
+
+    link.addEventListener('mouseleave', () => {
+      moveBallTo(currentActiveLink);
+    });
+
+    link.addEventListener('click', () => {
+      currentActiveLink = link;
+      moveBallTo(currentActiveLink);
+    });
+  });
+
+  function moveBallTo(targetLink) {
+    const targetRect = targetLink.getBoundingClientRect();
+    const containerRect = targetLink.parentElement.getBoundingClientRect();
+    const topPosition =
+      targetRect.top - containerRect.top + targetRect.height / 16;
+    const leftPosition = targetRect.left - containerRect.left - 10;
+    ball.style.top = `${topPosition}px`;
+    ball.style.left = `${leftPosition}px`;
+  }
+});
