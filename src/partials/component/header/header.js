ba6351a61f55291ebe91e10fb44639b20fa170ee
Gaondarech726 <anton.honcharenko2202@gmail.com>
2024-11-03 15:40:28 +0200

    delete new merges

--- src/partials/component/header/header.js
+++ src/partials/component/header/header.js
@@ -4,16 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
   navLinks.forEach(link => {
     link.addEventListener('click', event => {
       event.preventDefault();
-      // Видаляємо клас 'active' з усіх посилань
       navLinks.forEach(link => link.classList.remove('active'));
-
-      // Додаємо клас 'active' до натиснутого посилання
       event.target.classList.add('active');
-
-      // Отримуємо ID секції, до якої потрібно перейти
       const sectionId = event.target.getAttribute('href');
-
-      // Переходимо до секції
       document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
     });
   });
