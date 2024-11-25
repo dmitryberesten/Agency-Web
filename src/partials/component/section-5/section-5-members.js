6b3488eb4bd3ae5dddb5333a87b0047ba609d26d
K. L <121451594+madparrot69@users.noreply.github.com>
2024-11-25 20:57:56 +0200

    about-us-section

--- src/partials/component/section-5/section-5-members.js
+++ src/partials/component/section-5/section-5-members.js
@@ -1,105 +1,203 @@
 export const teamMembers = [
   {
-    name: "Khrystyna Stepenko",
-    role: "Scrum Master, Frontend Developer",
-    skillsResponsibilities: {
-      scrumManagement: {
-        tasks: [
-          "Creating tasks in Trello",
-          "Managing team workflows",
-          "Organizing team meetings"
-        ]
-      },
-      frontendDevelopment: {
-        tasks: [
-          "HTML and SCSS layout for Section 1 and Section 5 of a project",
-          "Creating JSON for Sections 3, 5, and 8",
-          "Developing adaptive layouts on Figma",
-          "Designing and implementing logos and modal windows"
-        ]
-      }
-    },
-    githubProfile: "https://github.com/madparrot69",
-    photo: "../../../img/section5/khrystyna.jpg"
-  },
-  {
-    name: "Serhii Zamorylo",
-    role: "Frontend Developer",
-    skillsResponsibilities: {
-      frontendDevelopment: {
-        tasks: [
-          "HTML and SCSS layout for Section 5",
-          "Integrated fonts and styles",
-          "Added hover animations",
-          "Created animation for the bell icon"
-        ]
-      }
-    },
-    githubProfile: "https://github.com/Serhii-Zamorylo23",
-    photo: "../../../img/section5/serhii.jpg"
-  },
-  {
-    name: "Anton Honcharenko",
-    role: "Tech Lead, Front-end Developer",
+    name: 'Dima Beresten',
+    role: 'Project Manager',
+    jobs: 'Project Management',
     skillsResponsibilities: {
       general: {
         tasks: [
-          "Quick error correction",
-          "Adaptability and mobility",
-          "Team collaboration and interaction"
-        ]
+          'Project management and task allocation',
+          'Team coordination',
+          'Workflow optimization',
+          'Quality control of completed tasks',
+        ],
+      },
+    },
+    githubProfile: 'https://github.com/dmitryberesten',
+    photo: '../../../img/section5/dima.jpg',
+  },
+  {
+    name: 'Anton Honcharenko',
+    role: 'Tech Lead, Frontend Developer',
+    jobs: 'Tech Management, Frontend Development',
+    skillsResponsibilities: {
+      general: {
+        tasks: [
+          'Quick error correction',
+          'Adaptability and mobility',
+          'Team collaboration and interaction',
+        ],
       },
       frontendDevelopment: {
         tasks: [
-          "HTML and SCSS layout for the Header",
-          "Developed a functional burger menu",
-          "Added hover animations"
-        ]
-      }
+          'HTML and SCSS layout for the Header',
+          'Developed a functional burger menu',
+          'Added hover animations',
+        ],
+      },
     },
-    githubProfile: "https://github.com/Gaondarech726",
-    photo: "../../../img/section5/anton.jpg"
+    githubProfile: 'https://github.com/Gaondarech726',
+    photo: '../../../img/section5/anton.jpg',
   },
   {
-    name: "Yaroslav Suntsev",
-    role: "Frontend Developer",
+    name: 'Khrystyna Stepenko',
+    role: 'Scrum Master, Frontend Developer',
+    jobs: 'Scrum Management, Frontend Development',
+    skillsResponsibilities: {
+      general: {
+        tasks: [
+          'Creating tasks, structured them and added to the Trello',
+          'Managing team workflows',
+          'Organizing team meetings, helped solve development problems',
+        ],
+      },
+      frontendDevelopment: {
+        tasks: [
+          'HTML, SCSS and JS layout for Section 1 and Section 5 of a project',
+          '',
+          'Developing adaptive layouts on Figma',
+          'Designing and implementing logo, loader and modal windows',
+        ],
+      },
+    },
+    githubProfile: 'https://github.com/madparrot69',
+    photo: '../../../img/section5/khrystyna.jpg',
+  },
+  {
+    name: 'Serhii Zamorylo',
+    role: 'Frontend Developer',
+    jobs: 'Team Management, Frontend Development',
+    skillsResponsibilities: {
+      general: {
+        tasks: ['Project architecture and technical decisions', '-', '-'],
+      },
+      frontendDevelopment: {
+        tasks: [
+          'HTML and SCSS layout for Section 5',
+          'Integrated fonts and styles',
+          'Added hover animations',
+          'Created animation for the bell icon',
+        ],
+      },
+    },
+    githubProfile: 'https://github.com/Serhii-Zamorylo23',
+    photo: '../../../img/section5/serhii.jpg',
+  },
+  {
+    name: 'Yaroslav Suntsev',
+    role: 'Frontend Developer',
+    jobs: 'Frontend Development',
     skillsResponsibilities: {
       frontendDevelopment: {
         tasks: [
-          "HTML and SCSS layout for the Footer",
-          "Added hover animations"
-        ]
-      }
+          'HTML and SCSS layout for the Footer',
+          'Added hover animations',
+        ],
+      },
     },
-    githubProfile: "https://github.com/cargo9",
-    photo: "../../../img/section5/yaroslav.jpg"
+    githubProfile: 'https://github.com/cargo9',
+    photo: '../../../img/section5/yaroslav.jpg',
   },
   {
-    name: "Dmytro Terletskyy",
-    role: "Frontend Developer",
+    name: 'Maria Kalinichenko',
+    role: 'Frontend Developer',
+    jobs: 'Frontend Development',
     skillsResponsibilities: {
       frontendDevelopment: {
         tasks: [
-          "HTML and SCSS layout for the Section 9"
-        ]
-      }
+          'HTML and SCSS layout for the Modal window contact us',
+          'Create favicon icons',
+        ],
+      },
     },
-    githubProfile: "https://github.com/Dremeret",
-    photo: "../../../img/section5/dmytro.jpg"
+    githubProfile: 'https://github.com/Mkqupy',
+    photo: '../../../img/section5/maria.jpg',
   },
   {
-    name: "Taras Shevchuk",
-    role: "Front-end Developer",
+    name: 'Igor Krasylnikov',
+    role: 'Frontend Developer',
+    jobs: 'Frontend Development',
     skillsResponsibilities: {
       frontendDevelopment: {
         tasks: [
-          "HTML and SCSS layout for Section 8",
-          "Created a slider",
-          "Responsive layout for Section 8"
-        ]
-      }
+          'HTML and SCSS layout for the Section 2',
+          'Developed a functional adaptive layout',
+          'Added SVG icons',
+        ],
+      },
     },
-    githubProfile: "https://github.com/Taras405",
-    photo: "../../../img/section5/taras.jpg"
-  }
+    githubProfile: 'https://github.com/Javus-Scriptus',
+    photo: '../../../img/section5/igor.jpg',
+  },
+  {
+    name: 'Eduard Kazyuk',
+    role: 'Frontend Developer',
+    jobs: 'Frontend Development',
+    skillsResponsibilities: {
+      frontendDevelopment: {
+        tasks: ['HTML and SCSS', 'Developed Section 7'],
+      },
+    },
+    githubProfile: 'https://github.com/GraceFeel1',
+    photo: '../../../img/section5/eduard.jpg',
+  },
+  {
+    name: 'Margarita Muminova',
+    role: 'Frontend Developer',
+    jobs: 'Frontend Development',
+    skillsResponsibilities: {
+      frontendDevelopment: {
+        tasks: [
+          'HTML and SCSS layout for the Section 6',
+          'Developed a functional adaptive layout',
+          'Added SVG icons',
+        ],
+      },
+    },
+    githubProfile: 'https://github.com/m4ggiee',
+    photo: '../../../img/section5/margarita.jpg',
+  },
+  {
+    name: 'Dmytro Terletskyy',
+    role: 'Frontend Developer',
+    jobs: 'Frontend Development',
+    skillsResponsibilities: {
+      frontendDevelopment: {
+        tasks: ['HTML and SCSS layout for the Section 9'],
+      },
+    },
+    githubProfile: 'https://github.com/Dremeret',
+    photo: '../../../img/section5/dmytro.jpg',
+  },
+  {
+    name: 'Taras Shevchuk',
+    role: 'Frontend Developer',
+    jobs: 'Frontend Development',
+    skillsResponsibilities: {
+      frontendDevelopment: {
+        tasks: [
+          'HTML and SCSS layout for Section 8',
+          'Created a slider',
+          'Responsive layout for Section 8',
+        ],
+      },
+    },
+    githubProfile: 'https://github.com/Taras405',
+    photo: '../../../img/section5/taras.jpg',
+  },
+  {
+    name: 'Svyatoslav Bykovets',
+    role: 'Frontend Developer',
+    jobs: 'Frontend Development',
+    skillsResponsibilities: {
+      frontendDevelopment: {
+        tasks: [
+          'HTML, CSS, SCSS, JavaScript, React',
+          'Worked on Section 4, including adaptive design',
+        ],
+      },
+    },
+    githubProfile: 'https://github.com/byebaji',
+    photo: '../../../img/section5/svyatoslav.jpg',
+  },
 ];
