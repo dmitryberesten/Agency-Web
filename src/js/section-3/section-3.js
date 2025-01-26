4cc19cec2fa7ed60d3ecde1b9bf12bc62a68753b
Serhii-Zamorylo23 <zamorylo@i.ua>
2025-01-26 12:32:58 +0200

    fix

--- src/js/section-3/section-3.js
+++
@@ -1,6 +0,0 @@
-const lernMoreBtn=document.querySelectorAll('#learnMore')
-lernMoreBtn.forEach(btn=>{
-    btn.addEventListener("click",()=>{
-        window.open=('https://t.me/agencywebicn','_blank')
-    })
-})
